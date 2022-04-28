b7696ff6755845bcea3c464be0b9ebe864be10f2
Suhasi94 <suhasini007kattimani@gmail.com>
2022-04-28 11:13:49 +0530

    adding files to git

---
+++ gitnotes.py
@@ -0,0 +1,11 @@
+# git add is used to add the file (untracked file(source code) to tracked file(staging area))
+# git commit(tracked file to saved /committed file(local repo))
+#git push (saved/commited file to central repo)
+#git pull(central repo to main source code it will be pulled)
+
+
+# git init
+# git config --user.name 
+#git status
+#git add filename
+#git rm --cached filename
